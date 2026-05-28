fed1e952b7ed460bc5ed602c645db3714712c59f
ndammo <raikinmuj3@gmail.com>
2026-05-29 00:04:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -343,7 +343,7 @@ async function initTelegramApp() {
         
         setTimeout(() => {
             const top = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--tg-safe-area-inset-top')) || 50;
-            document.querySelector('.header').style.paddingTop = (top + 20) + 'px';
+            document.querySelector('.header').style.paddingTop = (top + 30) + 'px';
         }, 400);
         
         // Отключаем вертикальные свайпы (чтобы не схлопывалось)
