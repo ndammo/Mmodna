6041ddc26c0c7f9dcece5c1e46b7113ea386e0ba
ndammo <raikinmuj3@gmail.com>
2026-06-11 21:23:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -466,7 +466,7 @@ function showArenaClosedModal() {
 }
 
 // Расписание арены (UTC+3)
-const ARENA_SCHEDULE_CLIENT = [[10, 20], [21, 02]];
+const ARENA_SCHEDULE_CLIENT = [[10, 20], [21, 22]];
 
 function isArenaOpenClient() {
     const nowUTC = new Date();
